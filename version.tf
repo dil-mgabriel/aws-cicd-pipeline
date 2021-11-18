363694b721e14c9bcb7b8994c1bef0f28250816b
soho yeo <syeo@diligent.com>
2021-11-18 13:45:51 -0500

    fixed var

--- version.tf
+++ version.tf
@@ -22,8 +22,8 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  access_key = $AWS_ACCESS_KEY_ID
-  secret_key = $AWS_SECRET_ACCESS_KEY
-  token = $AWS_SESSION_TOKEN
+  # access_key = $AWS_ACCESS_KEY_ID
+  # secret_key = $AWS_SECRET_ACCESS_KEY
+  # token = $AWS_SESSION_TOKEN
 
 }
