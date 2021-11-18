2734056bd927b6f1b021aead4a64a184bda8bc58
soho yeo <syeo@diligent.com>
2021-11-18 11:06:59 -0500

    fixed var

--- version.tf
+++ version.tf
@@ -15,8 +15,8 @@ terraform {
     # dynamodb_table = "terraform-lock-file"
     key = "path/terraform.tfstate"
     region = "us-east-1"
-    access_key = "{{ssm:Access_Key_Id}}"
-    secret_key = "{{ssm:Secret_access_key}}"
+    # access_key = "{{ssm:Access_Key_Id}}"
+    # secret_key = "{{ssm:Secret_access_key}}"
     # token = "{{session_token}}"
     }
 }
