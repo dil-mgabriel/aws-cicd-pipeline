c659b113a60b967cda4f7bfe029a6f5cbd00a51a
soho yeo <syeo@diligent.com>
2021-11-18 13:36:56 -0500

    fixed var

--- version.tf
+++ version.tf
@@ -1,14 +1,13 @@
 #configure the AWS provider
-provider "aws" {
-  region = "us-east-1"
-  version = "v2.70.0"
-  access_key = $AWS_ACCESS_KEY_ID
-  secret_key = $AWS_SECRET_ACCESS_KEY
-  token = $AWS_SESSION_TOKEN
 
-}
 
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 
   backend "s3" {
     bucket = "aws-cicd-pipeline-terraform-state" 
@@ -19,4 +18,12 @@ terraform {
     secret_key = $AWS_SECRET_ACCESS_KEY
     token = $AWS_SESSION_TOKEN
     }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  access_key = $AWS_ACCESS_KEY_ID
+  secret_key = $AWS_SECRET_ACCESS_KEY
+  token = $AWS_SESSION_TOKEN
+
 }
