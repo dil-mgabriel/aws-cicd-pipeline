b10aed481bcc52008604064c263fafc348712228
soho yeo <syeo@diligent.com>
2021-11-19 10:52:52 -0500

    fixed data_store

--- version.tf
+++ version.tf
@@ -17,11 +17,11 @@ terraform {
 
   backend "s3" {
     bucket = "aws-cicd-pipeline-terraform-state" 
-    dynamodb_table = "terraform-lock-file"
+    # dynamodb_table = "terraform-lock-file"
     key = "path/terraform.tfstate"
     region = "us-east-1"
-    access_key = data.aws_ssm_parameter.access_key.value
-    secret_key = data.aws_ssm_parameter.secret_key.value
+    # access_key = data.aws_ssm_parameter.access_key.value
+    # secret_key = data.aws_ssm_parameter.secret_key.value
     # session_token = "${data.aws_ssm_parameter.session_token.value}"
     }
 }
