b244b4094a9fe7489108e1e0760b4658c1c1a15d
soho yeo <syeo@diligent.com>
2021-11-19 11:36:12 -0500

    fixed data_store

--- version.tf
+++ version.tf
@@ -1,8 +1,8 @@
 #configure the AWS provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "${data.aws_ssm_parameter.access_key_db123.value}"
-  secret_key = "${data.aws_ssm_parameter.secret_key_db123.value}"
+  # access_key = "${data.aws_ssm_parameter.access_key_db123.value}"
+  # secret_key = "${data.aws_ssm_parameter.secret_key_db123.value}"
   # session_token = data.aws_ssm_parameter.session_token.value
 
 }
@@ -20,10 +20,10 @@ terraform {
     dynamodb_table = "terraform-lock-file"
     key = "path/terraform.tfstate"
     region = "us-east-1"
-    role_arn      = "arn:aws:iam::117005682321:role/pipeline_role"
-    # access_key = data.aws_ssm_parameter.access_key.value
-    # secret_key = data.aws_ssm_parameter.secret_key.value
-    # session_token = "${data.aws_ssm_parameter.session_token.value}"
+    # role_arn      = "arn:aws:iam::117005682321:role/pipeline_role"
+    # # access_key = data.aws_ssm_parameter.access_key.value
+    # # secret_key = data.aws_ssm_parameter.secret_key.value
+    # # session_token = "${data.aws_ssm_parameter.session_token.value}"
     }
 }
 
